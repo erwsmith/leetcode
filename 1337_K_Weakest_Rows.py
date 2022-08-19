62ac3afe6996ec3649361c9c7f9d16d65048fb58
Eric Smith <erwsmith@gmail.com>
2022-08-19 16:52:38 -0600

    done

--- 1337_K_Weakest_Rows.py
+++ 1337_K_Weakest_Rows.py
@@ -1,23 +1,20 @@
-import numpy as np
+from operator import itemgetter
 
 class Solution:
     def kWeakestRows(self, mat: list[list[int]], k: int) -> list[int]:
-        soldiers = []
-        weakest = []
-        for i in range(len(mat[0])):
-            soldiers.append(sum(mat[i]))
-        print(soldiers)
-        while len(soldiers) >= k:
-            self.popMin(soldiers, weakest)
-        return soldiers, weakest
-    
-    def popMin(self, soldiers, weakest):
-        index_min = np.argmin(soldiers)
-        soldiers.pop(index_min)
-        weakest.append(index_min)
-        return soldiers, weakest
-
-
+        ranks = []
+        # create list of tuples, preserving the original index
+        for i, row in enumerate(mat):
+            ranks.append((i, sum(row)))
+        # sort list of tuples using second item as key
+        ranks = sorted(ranks, key=itemgetter(1))
+        # take slice of ranks to get just first k items
+        ranks = ranks[:k]
+        output = []
+        # create list of just the original indices
+        for i in range(len(ranks)):
+            output.append(ranks[i][0])
+        return output
 
 solution = Solution()
 mat = [[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]]
