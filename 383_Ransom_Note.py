e0c475e57e4ef76a1581be6f797a955623b60407
Eric Smith <erwsmith@gmail.com>
2022-08-19 13:16:50 -0600

    formatting rm unused stuff

--- 383_Ransom_Note.py
+++ 383_Ransom_Note.py
@@ -1,14 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         note, mag = ransomNote, magazine
-        print(note, mag)
-
         for magletter in mag:
             for noteletter in note:
                 if noteletter in mag:
                     note = note.replace(noteletter, '', 1)
                     mag = mag.replace(noteletter, '', 1)
-                    print(note, mag)
         
         return len(note) == 0
 
