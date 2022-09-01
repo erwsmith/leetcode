d8e1bf8a5504dbfa646014b1debee6f58b62ad9f
Eric Smith <erwsmith@gmail.com>
2022-09-01 08:50:21 -0600

    rm errant '

--- 1084_Sales_Analysis_III.sql
+++ 1084_Sales_Analysis_III.sql
@@ -31,7 +31,7 @@ WHERE product_id IN (
     SELECT product_id FROM Sales
     WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'
 AND product_id NOT IN (
-    SELECT product_id FROM Sales'
+    SELECT product_id FROM Sales
     WHERE sale_date > '2019-03-31')
 AND product_id NOT IN (
     SELECT product_id FROM Sales
