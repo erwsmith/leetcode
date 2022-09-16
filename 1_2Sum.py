9011886e3b19a9e5c6355b5c491a020bc040f620
Eric Smith <erwsmith@gmail.com>
2022-09-16 10:15:35 -0600

    working solution, found on leet discussion board

--- 1_2Sum.py
+++ 1_2Sum.py
@@ -5,14 +5,11 @@
 # You can return the answer in any order.
 
 def twoSum(nums, target):
-    output = []
-    for i in range(len(nums)):
-        if i not in output:
-            for j in range(1, len(nums)):
-                if j != i and j not in output and nums[i] + nums[j] == target:
-                    output.append(i)
-                    output.append(j)
-    return output
+    for i, value in enumerate(nums):
+        remaining = target - nums[i]
+        nums[i] = None
+        if remaining in nums:
+            return [i, nums.index(remaining)]
 
 nums = [3,2,4]
 target = 6
