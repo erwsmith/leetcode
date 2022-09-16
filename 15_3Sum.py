66387b6262929aa1d54de51c0f6e1b01b29069d1
Eric Smith <erwsmith@gmail.com>
2022-09-16 09:49:22 -0600

    remove breaks by using != instead

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -7,15 +7,13 @@ nums = [-1,0,1,2,-1,-4]
 output = []
 for i in range(len(nums)):
     for j in range(1, len(nums)):
-        if j == i:
-            break
-        for k in range(2, len(nums)):
-            if k == i or k == j:
-                break
-            if nums[i] + nums[j] + nums[k] == 0 and i != j and i != k and j !=k:
-                element = [nums[i], nums[j], nums[k]]
-                element.sort()
-                if element not in output:
-                    output.append(element)
+        if j != i:
+            for k in range(2, len(nums)):
+                if k != i and k != j:
+                    if nums[i] + nums[j] + nums[k] == 0 and i != j and i != k and j !=k:
+                        element = [nums[i], nums[j], nums[k]]
+                        element.sort()
+                        if element not in output:
+                            output.append(element)
 
 print(output)
