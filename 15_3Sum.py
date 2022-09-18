ba3ef73e01c097d8b1c59460a12e530df96c099c
Eric Smith <erwsmith@gmail.com>
2022-09-18 11:31:41 -0600

    in progress

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -3,17 +3,20 @@
 # and nums[i] + nums[j] + nums[k] == 0.
 # Notice that the solution set must not contain duplicate triplets.
 
-nums = [-1,0,1,2,-1,-4]
-output = []
-for i in range(len(nums)):
-    for j in range(1, len(nums)):
-        if j != i:
-            for k in range(2, len(nums)):
-                if k != i and k != j:
-                    if nums[i] + nums[j] + nums[k] == 0 and i != j and i != k and j !=k:
-                        element = [nums[i], nums[j], nums[k]]
-                        element.sort()
-                        if element not in output:
-                            output.append(element)
+def threeSum(nums):
+    output = []
+    for i, value in enumerate(nums):
+        target = -nums[i]
+        print(target)
+        # output.append(twoSum(nums, target))
+        # return output
 
-print(output)
+def twoSum(nums, target):
+    for k, value in enumerate(nums):
+        remaining = target - nums[k]
+        nums[k] = None
+        if remaining in nums:
+            return [k, nums.index(remaining)]
+
+nums = [-1,0,1,2,-1,-4]
+print(threeSum(nums))
