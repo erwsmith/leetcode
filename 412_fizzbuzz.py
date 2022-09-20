8fc9685971fe4ee3326a0323b5f6a1468b6193a4
Eric Smith <erwsmith@gmail.com>
2022-09-20 09:26:34 -0600

    format

--- 412_fizzbuzz.py
+++ 412_fizzbuzz.py
@@ -2,7 +2,7 @@ class Solution:
     def fizzBuzz(self, n: int):
         result = []
         for num in range(1, n + 1):
-            if num % 3 ==0 and num % 5 == 0:
+            if num % 3 == 0 and num % 5 == 0:
                 result.append("FizzBuzz")
             elif num % 3 == 0:
                 result.append("Fizz")
