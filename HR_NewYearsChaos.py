1f5bfcb995fbc84a0788682cd2afd72b3a48afec
Eric Smith <erwsmith@gmail.com>
2022-10-12 11:12:36 -0600

    initial commit

---
+++ HR_NewYearsChaos.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumBribes' function below.
+#
+# The function accepts INTEGER_ARRAY q as parameter.
+#
+
+def minimumBribes(q):
+    return q
