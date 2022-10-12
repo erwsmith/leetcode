fe714927f21aa68d60076c5fe0523dd4b57e5bdd
Eric Smith <erwsmith@gmail.com>
2022-10-12 11:01:51 -0600

    working, all tests passed, collections Counter

--- HR_SherlockValidString.py
+++ HR_SherlockValidString.py
@@ -1,72 +1,59 @@
 import time
 import csv 
+from collections import Counter
 
 def isValid(s):
-    mismatch_count = 0
-    count_list = []
-    for i in range(len(s) - 1):
-        if mismatch_count > 2:
-            # print('mismatch_count > 1')
-            return 'NO'
-        if s.count(s[i]) != s.count(s[i+1]) and s.count(s[i]) not in count_list and s.count(s[i+1]) not in count_list:
-            count_list.append(s.count(s[i]))
-            count_list.append(s.count(s[i+1]))
-            mismatch_count += 1
-            # print(mismatch_count, i, s[i], s.count(s[i]), s[i+1], s.count(s[i+1]))
-    
-    freq = {}
-    for letter in s:
-        freq[letter] = s.count(letter)
-    # print(freq)
-    
+    # Create dictionary using imported function (this is the key to fast dict creation for this challenge)
+    freq = Counter(s)
+    # get max & min
     maxval = max(freq.values())
     minval = min(freq.values())
-    
+    # if all values are the same
     if maxval == minval:
-        # print('maxval = minval')
         return 'YES'
-    
     max_counter = min_counter = 0
     for val in freq.values():
+        # if there are more than 2 value counts
         if min_counter > 1 and max_counter > 1:
-            # print('min_counter and max_counter > 1')
             return 'NO'
         if val == maxval:
             max_counter += 1
         elif val == minval:
             min_counter += 1
-    
+    # Other checks ...
     if max_counter + min_counter != len(freq):
-        # print('More than 2 counts')
         return 'NO'
     elif min_counter != 1 and max_counter != 1:
-        # print('No 1s found')
         return 'NO'
     elif max_counter == maxval - minval == 1:
-        # print('max_counter yes')
         return 'YES'
     elif min_counter == minval == 1:
-        # print('min_counter yes')
         return 'YES'
-    # print('No yes outcomes found, end of function')
     return 'NO'
 
-# longstring = ''
-# with open('sherTest13.csv') as csvfile:
-#     reader = csv.reader(csvfile)
-#     for row in reader:
-#         longstring = row
 
-# # timing
-# tic = time.perf_counter()
-# isValid(longstring[0])
-# toc = time.perf_counter()
-# print(toc - tic)
+'''
+TESTING 
+'''
 
+# Get longstring from csv
+longstring = ''
+with open('sherTest13.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        longstring = row
+
+# Timing
+tic = time.perf_counter()
+isValid(longstring[0])
+toc = time.perf_counter()
+print(toc - tic)
+
+# Results should be True if they pass
 print(f'{"NO" == isValid("aabbcd")} (Sample Test case 0)')
 print(f'{"NO" == isValid("aabbccddeefghi")} (Sample Test case 1)')
 print(f'{"YES" == isValid("abcdefghhgfedecba")} (Sample Test case 2)')
 print('YES' == isValid('ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd'))
 print('NO' == isValid('aaaabbcc'))
 print('NO' == isValid('abbccc'))
-# print('NO' == isValid(longstring[0]))
+print('YES' == isValid(longstring[0]))
